72aa571c7c20eeff2bd245feb361c073467dffe5
JP <j.lacap212@gmail.com>
2018-03-11 16:56:03 -0400

    FB Share

--- Compliplenty/app/src/main/java/hxgstudio/compliplenty/StartApp.java
+++ Compliplenty/app/src/main/java/hxgstudio/compliplenty/StartApp.java
@@ -1,8 +1,10 @@
 package hxgstudio.compliplenty;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,18 +13,30 @@ import android.widget.Toast;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.facebook.share.Sharer;
+import com.facebook.share.model.ShareHashtag;
+import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.widget.ShareButton;
+import com.facebook.share.widget.ShareDialog;
 
 public class StartApp extends AppCompatActivity {
 
     private TextView info;
     private LoginButton loginButton;
     private CallbackManager callbackManager;
+    private ShareDialog shareDialog;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        callbackManager = CallbackManager.Factory.create();
+        shareDialog = new ShareDialog(this);
 
         setContentView(R.layout.activity_startapp);
 
@@ -30,15 +44,16 @@ public class StartApp extends AppCompatActivity {
         profile = (Button) findViewById(R.id.icon_profile);
 
         callbackManager = CallbackManager.Factory.create();
-        loginButton = (LoginButton)findViewById(R.id.login_button);
-        info = (TextView)findViewById(R.id.info);
+        logout = (LoginButton) findViewById(R.id.login_button);
+        info = (TextView) findViewById(R.id.info);
 
         contacts = (Button) findViewById(R.id.icon_contacts);
         up = (Button) findViewById(R.id.icon_up);
-        share = (Button) findViewById(R.id.icon_share);
+
         down = (Button) findViewById(R.id.icon_down);
         question = (Button) findViewById(R.id.icon_question);
         random = (Button) findViewById(R.id.icon_random);
+
 
         profile.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -48,22 +63,13 @@ public class StartApp extends AppCompatActivity {
             }
         });
 
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+        logout.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onSuccess(LoginResult loginResult) {
-                Intent intent;
-                intent = new Intent(StartApp.this, LogIn.class);
-                StartApp.this.startActivity(intent);
-            }
-
-            @Override
-            public void onCancel() {
-                info.setText("Logout attempt canceled.");
-            }
-
-            @Override
-            public void onError(FacebookException e) {
-                info.setText("Logout attempt failed.");
+            public void onClick(View view){
+                LoginManager.getInstance().logOut();
+                Intent login = new Intent(StartApp.this, LogIn.class);
+                        startActivity(login);
+                finish();
             }
         });
 
@@ -79,12 +85,25 @@ public class StartApp extends AppCompatActivity {
                 Toast.makeText(StartApp.this, "Upvoted!!", Toast.LENGTH_SHORT).show();
             }
         });
-        share.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Toast.makeText(StartApp.this, "Share", Toast.LENGTH_SHORT).show();
 
+        ShareButton shareButton = (ShareButton) findViewById(R.id.icon_share);
+        shareButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ShareLinkContent content = new ShareLinkContent.Builder().build();
+                shareDialog.show(content);
+
+                    /*if (ShareDialog.canShow(ShareLinkContent.class)) {
+                    ShareLinkContent content = new ShareLinkContent.Builder()
+                            .setShareHashtag(new ShareHashtag.Builder()
+                                    .setHashtag("#Compliplenty")
+                            .build())
+                            .setQuote("A compliment a day, keeps the sadness at bay")
+                            .build();
+                    shareDialog.show(content);*/
             }
         });
+
         down.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Toast.makeText(StartApp.this, "Downvoted!!", Toast.LENGTH_SHORT).show();
@@ -110,4 +129,19 @@ public class StartApp extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         callbackManager.onActivityResult(requestCode, resultCode, data);
     }
+
+
+    private FacebookCallback<Sharer.Result> callback = new FacebookCallback<Sharer.Result>() {
+        @Override
+        public void onSuccess(Sharer.Result result) {
+        }
+
+        @Override
+        public void onCancel() {
+        }
+
+        @Override
+        public void onError(FacebookException error) {
+        }
+    };
 }
