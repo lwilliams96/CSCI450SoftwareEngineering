bb09bb0030e4f68ccb8f4914682ddec5bfd24aea
lwilliams96 <lwilliams2@eagles.bridgewater.edu>
2018-04-23 11:26:30 -0400

    Almost done

---
+++ Compliplenty/app/src/main/java/hxgstudio/compliplenty/OptionDialog.java
@@ -0,0 +1,103 @@
+package hxgstudio.compliplenty;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import static android.app.Activity.RESULT_OK;
+import static com.facebook.FacebookSdk.getApplicationContext;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link //OptionDialog.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link //OptionDialog#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class OptionDialog extends DialogFragment {
+    //@Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage("Take New Photo or Choose Existing?")
+                .setPositiveButton("Take New Photo", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // Send the positive button event back to the host activity
+                        mListener.onDialogPositiveClick(OptionDialog.this);
+                    }
+                })
+                .setNegativeButton("Choose Photo", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // Send the negative button event back to the host activity
+                        mListener.onDialogNegativeClick(OptionDialog.this);
+                    }
+                });
+        return builder.create();
+    }
+
+    public interface OptionDialogListener {
+        public void onDialogPositiveClick(DialogFragment dialog);
+        public void onDialogNegativeClick(DialogFragment dialog);
+    }
+
+    // Use this instance of the interface to deliver action events
+    OptionDialogListener mListener;
+
+    // Override the Fragment.onAttach() method to instantiate the NoticeDialogListener
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        // Verify that the host activity implements the callback interface
+        try {
+            // Instantiate the NoticeDialogListener so we can send events to the host
+            mListener = (OptionDialogListener) activity;
+        } catch (ClassCastException e) {
+            // The activity doesn't implement the interface, throw exception
+            throw new ClassCastException(activity.toString()
+                    + " must implement NoticeDialogListener");
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch(requestCode) {
+            case 1234:
+                if(resultCode == RESULT_OK){
+                    Uri selectedImage = data.getData();
+                    String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+                    Cursor cursor = getApplicationContext().getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+                    cursor.moveToFirst();
+
+                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                    String filePath = cursor.getString(columnIndex);
+                    cursor.close();
+
+
+                    Bitmap takenPhoto = BitmapFactory.decodeFile(filePath);
+                    //ImageView img = (ImageView) findViewById(R.id.profilePicture);
+                    //img.setImageBitmap(takenPhoto);
+                }
+        }
+    };
+}
--- Compliplenty/app/src/main/java/hxgstudio/compliplenty/Profile.java
+++ Compliplenty/app/src/main/java/hxgstudio/compliplenty/Profile.java
@@ -1,15 +1,29 @@
 package hxgstudio.compliplenty;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 
-public class Profile extends AppCompatActivity {
+import static com.facebook.FacebookSdk.getApplicationContext;
+
+public class Profile extends FragmentActivity implements OptionDialog.OptionDialogListener {
 
 
     @Override
@@ -26,7 +40,8 @@ public class Profile extends AppCompatActivity {
 
         editProfile.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Toast.makeText(Profile.this, "Edit Profile", Toast.LENGTH_SHORT).show();
+                DialogFragment newFragment = new OptionDialog();
+                newFragment.show(getSupportFragmentManager(), "missiles");
             }
         });
 
@@ -38,12 +53,12 @@ public class Profile extends AppCompatActivity {
             }
         });
 
-        final Intent profileToLikeList = new Intent (this, LikeList.class);
+        final Intent profileToLikeList = new Intent(this, LikeList.class);
         viewLikeList.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 String putExtraHelper;
                 ArrayList<String> listOfLikes = new ArrayList<String>();
-                for(int i = 0; i < listOfLikes.size(); i++) {
+                for (int i = 0; i < listOfLikes.size(); i++) {
                     putExtraHelper = "Compliment ";
                     putExtraHelper = putExtraHelper + i;
                     profileToLikeList.putExtra(putExtraHelper, "" + listOfLikes.get(i));
@@ -53,5 +68,55 @@ public class Profile extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void showNoticeDialog() {
+        // Create an instance of the dialog fragment and show it
+        DialogFragment dialog = new OptionDialog();
+        dialog.show(getSupportFragmentManager(), "NoticeDialogFragment");
+    }
+
+    // The dialog fragment receives a reference to this Activity through the
+    // Fragment.onAttach() callback, which it uses to call the following methods
+    // defined by the NoticeDialogFragment.NoticeDialogListener interface
+    @Override
+    public void onDialogPositiveClick(DialogFragment dialog) {
+        // User touched the dialog's positive button
+        Intent intent;
+        intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        startActivity(intent);
+    }
+
+    @Override
+    public void onDialogNegativeClick(DialogFragment dialog) {
+        // User touched the dialog's negative button
+        Intent i = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        final int ACTIVITY_SELECT_IMAGE = 1234;
+        startActivityForResult(i, ACTIVITY_SELECT_IMAGE);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case 1234:
+                if (resultCode == RESULT_OK) {
+                    try {
+                        final Uri imageUri = data.getData();
+                        final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                        final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                        ImageView img = (ImageView) findViewById(R.id.imageView_profile);
+                        img.setImageBitmap(selectedImage);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                        Toast.makeText(Profile.this, "Something went wrong", Toast.LENGTH_LONG).show();
+                    }
+
+                } else {
+                    Toast.makeText(Profile.this, "You haven't picked Image", Toast.LENGTH_LONG).show();
+                }
+        }
+        ;
     }
 }
